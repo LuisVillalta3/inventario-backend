98834b2c7208eb9533ea57de6757e9e4cd723d6e
LuisVillalta3 <luisvillalta3@gmail.com>
2023-06-05 20:13:34 -0600

    Seeders

---
+++ database/factories/WarehouseFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Warehouse>
+ */
+class WarehouseFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'code' => $this->faker->word,
+            'nombre' => $this->faker->name,
+            'direccion' => $this->faker->address,
+            'telefono' => $this->faker->phoneNumber,
+            'fax' => $this->faker->phoneNumber,
+            'disponible' => true,
+        ];
+    }
+}
